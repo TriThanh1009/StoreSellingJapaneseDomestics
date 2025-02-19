0c0dcdd3202ea139662cc4c40fcba35a3811e38a
Trí Thanh <92020142+TriThanh1009@users.noreply.github.com>
2025-02-19 23:38:14 +0700

    vietqr pay ( just generate qr for payment)

---
+++ StoreSellingJapaneseDomestics/Controllers/VNPayController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SSJD.ViewModel.GeneralViewModel.VietQRPay;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StoreSellingJapaneseDomestics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VNPayController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public VNPayController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("generate-qr")]
+        public IActionResult GenerateQR(string bankCode, string accountNumber, decimal amount, string content)
+        {
+            string qrUrl = $"https://img.vietqr.io/image/{bankCode}-{accountNumber}-qr_only.png?amount={amount}&addInfo={Uri.EscapeDataString(content)}";
+            return Ok(new { QrImageUrl = qrUrl });
+        }
+
+    }
+}
